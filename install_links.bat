92981f817d96aaef160932550ddbf0b2dad92d2e
shycherry <vinpowful@free.fr>
2014-06-24 00:03:07 +0200

    wip

--- install_links.bat
+++ install_links.bat
@@ -1,2 +1,3 @@
 mklink /D .\node_modules\itemTagsDB ..\..\itemTagsDB
 mklink /D .\node_modules\itemTagsWatcher ..\..\itemTagsWatcher
+mklink /D .\bower_components\shy-elt-freewall ..\..\shy-elt-freewall
