87497e1eff6e061fdf9e0df02dd15d40d4e7512b
shycherry <vinpowful@free.fr>
2014-08-08 21:57:28 +0200

    add itemTagsSniffer dependency

--- install_links.bat
+++ install_links.bat
@@ -1,3 +1,4 @@
 mklink /D .\node_modules\itemTagsDB ..\..\itemTagsDB
 mklink /D .\node_modules\itemTagsWatcher ..\..\itemTagsWatcher
+mklink /D .\node_modules\itemTagsSniffer ..\..\itemTagsSniffer
 mklink /D .\bower_components\shy-elt-freewall ..\..\shy-elt-freewall
